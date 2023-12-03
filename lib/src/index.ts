ebf94b93c5e5bf430a167e7650baea065e3367b5
Geoff Cox <geoff.cox@live.com>
2023-12-02 23:14:04 -0800

    Update index.ts

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,4 @@
 export type { TrieSearchFoundRange} from './trieSearch';
 export { trieSearch, trieSearchArray, trieSearchString} from './trieSearchApi';
 export { createWordIterator} from './wordIterator';
+export { createCharacterIterator} from './characterIterator';
